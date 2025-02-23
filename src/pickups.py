72e2040f437c9b35207cff5694a69dd4adbe8a05
MagnusHansson <magnus.p.hansson@gmail.com>
2025-02-23 11:05:06 +0100

    Version 2, punkt J

--- src/pickups.py
+++ src/pickups.py
@@ -10,7 +10,7 @@ class Item:
         return self.symbol
 
 
-pickups = [Item("carrot"), Item("apple", 20, ""), Item("strawberry", 20, "s"), Item("cherry", 20, "c"), Item("watermelon"), Item("radish"), Item("cucumber"), Item("meatball")]
+pickups = [Item("carrot"), Item("apple", 20, ""), Item("strawberry", 20, "s"), Item("cherry", 20, "c"), Item("watermelon"), Item("radish"), Item("cucumber"), Item("meatball"), Item("spade", 0, "S")]
 
 
 def randomize(grid):
--- src/player.py
+++ src/player.py
@@ -18,10 +18,20 @@ class Player:
         self.pos_y += dy
 
     def can_move(self, x, y, grid):
-        maybe_wall = grid.get(self.pos_x + x, self.pos_y + y)
-        if maybe_wall == "■": #Returnerar att det inte går att flytta om det är ett väggtecken i vägen
-            return False
-        return True
+        new_x = self.pos_x + x
+        new_y = self.pos_y + y
+        maybe_wall = grid.get(new_x, new_y)
+        if new_x == 0 or new_y == 0 or new_x == (grid.width - 1) or new_y == (grid.height - 1):
+            return False #Returnera False om spelaren provar att gå utanför spelplanen. Förhindrar också att vi kan använda spaden på ytterväggarna.
+        else:
+            if maybe_wall == "■": #Kollar om det finns ett väggtecken i vägen.
+                if self.inventory.is_in_storage("spade"):
+                    self.inventory.remove_from_inventory("spade", 1) #Ta bort en spade ur inventory eftersom vi kommer använda den på nästa rad.
+                    grid.clear(new_x, new_y) #Ta bort väggelement.
+                    return True #Returnera True eftersom vi har kunnat ta bort väggelementet med spaden.
+                else:
+                    return False #Ingen spade returnera False, väggelement i vägen.
+            return True #Returnera True, inget väggelement i vägen.
 
     def move_right(self, g):
         if self.can_move(1, 0, g):
